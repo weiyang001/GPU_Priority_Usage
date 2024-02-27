7d4a766ba33a78fa1d303323718e6f43ee8c9133
weiyang001 <weiyu2021@gmail.com>
2024-02-27 14:44:22 +0800

    refresh code

--- train.sh
+++ train.sh
@@ -2,16 +2,18 @@
 var=0
 ocp_memory=${1:-500}
 while [ $var -eq 0 ]
-echo 'waiting for available gpu...'
+# echo 'waiting for available gpu...'
 do
     count=0
     for i in $(nvidia-smi --query-gpu=memory.used --format=csv,noheader,nounits)
     do
-        echo 'GPU'$i''
-        if [ $i -lt $ocp_memory ]
+        # echo 'GPU'$i''
+        size=$((81920-$i))
+        # echo ''$size''
+        if [ $i -lt $size ]
         then
             echo 'GPU'$count' is avaiable'
-            CUDA_VISIBLE_DEVICES=$count python /U_PZL2022KF0005/home/wangweiyu/Second/train/train01/train.py --cuda --gpu 6 --epoches 4 --batchSize 16 --lr 1e-4 --style_content_loss --recon_loss --tv_loss --recon_ws_ssim_loss --ws_ssim_loss --temporal_loss --data_sigma --data_w
+            CUDA_VISIBLE_DEVICES=$count python train.py
             var=1
             break
         fi
